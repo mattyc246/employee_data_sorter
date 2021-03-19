b8c532e46a6749658d7327b12844dd6c0cc6790c
Matthew Cross <mattyc246@gmail.com>
2021-03-19 22:11:37 +0800

    created base structure for employee context

--- src/context/employeeContext.ts
+++ src/context/employeeContext.ts
@@ -1,3 +1,4 @@
+import { createContext, useState } from 'react'
 import { Employee } from "../interfaces/Employee";
 
 export interface EmployeeContextData {
@@ -5,3 +6,24 @@ export interface EmployeeContextData {
   fetchingEmployees: boolean;
   fetchEmployees: () => void;
 }
+
+export const employeeContextDefaultVal: EmployeeContextData = {
+  employees: [],
+  fetchingEmployees: false,
+  fetchEmployees: () => null
+}
+
+export const useEmployeeContext = (): EmployeeContextData => {
+  const [employees, setEmployees] = useState<Employee[]>([])
+  const [fetchingEmployees, setFetchingEmployees] = useState<boolean>(false)
+
+  const fetchEmployees = () => {}
+
+  return {
+    employees,
+    fetchingEmployees,
+    fetchEmployees
+  }
+}
+
+export const EmployeeContext = createContext<EmployeeContextData>(employeeContextDefaultVal)
