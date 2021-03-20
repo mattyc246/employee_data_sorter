c34855158ba17cee5869560fd35a4ce55a612f77
Matthew Cross <mattyc246@gmail.com>
2021-03-20 12:38:46 +0800

    styled and added in icon

--- src/components/UserCount.tsx
+++ src/components/UserCount.tsx
@@ -1,3 +1,5 @@
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { FunctionComponent } from 'react'
 import { Employee } from '../interfaces/Employee'
 
@@ -8,8 +10,9 @@ export interface UserCountProps {
 const UserCount: FunctionComponent<UserCountProps> = ({ employees }: UserCountProps) => {
   return (
     <div className="w-100 card rounded">
-      <div className="card-body d-flex justify-content-between">
-        <h1>{employees.length}</h1>
+      <div className="card-body d-flex justify-content-around align-items-center px-3">
+        <FontAwesomeIcon className="fa__icon" icon={faUser} />
+        <h2 className="m-0">{employees.length}</h2>
       </div>
     </div>
   )
